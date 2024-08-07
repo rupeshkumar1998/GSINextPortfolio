fe23e06164f8035fb2c9ef65daef775910d06e92
ayuSh0614 <ayusharma051@gmail.com>
2024-08-07 12:29:48 +0530

    MyService Component added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import MyService from '../Components/MyService'
 const Home = () => {
   return (
+  <>
    <Header/>
+   <MyService></MyService>
+   </>
   )
 }
 export default Home
