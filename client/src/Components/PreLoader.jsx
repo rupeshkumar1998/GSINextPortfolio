c3e78882b52468de1f973f228023f3273693085b
rupeshkumar1998 <rupeshkumarricky09@gmail.com>
2024-08-11 23:07:03 +0530

    PreLoader added

--- client/src/Components/PreLoader.jsx
+++ client/src/Components/PreLoader.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const PreLoader = () => {
-  return (
-    <div>PreLoader</div>
-  )
-}
+  const [isSlidingUp, setIsSlidingUp] = useState(false);
 
-export default PreLoader
+  useEffect(() => {
+    setTimeout(() => {
+      setIsSlidingUp(true);
+    }, 2000);
+  }, []);
+
+  return (
+    <div
+      className={`fixed inset-0 bg-black flex items-center justify-center z-50 transition-transform duration-1000 ${
+        isSlidingUp ? "transform -translate-y-full" : ""
+      }`}
+    //   style={{
+    //     clipPath: "ellipse(100% 85% at 50% 100%)", // Adjust the curve here
+    //   }}
+    >
+      <div className="text-white text-4xl font-bold animate-pulse">
+        Loading
+      </div>
+    </div>
+  );
+};
+
+export default PreLoader;
